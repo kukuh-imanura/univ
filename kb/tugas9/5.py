bd7fa04f8c791eb71275bc4da96c237b4c172d2f
kukuh-imanura <88927405+kukuh-imanura@users.noreply.github.com>
2022-10-06 07:51:43 +0800

    Add files via upload

---
+++ kb/tugas9/5.py
@@ -0,0 +1,5 @@
+print("CELCIUS to FARENHEIT")
+print("Masukkan Nilai Celcius: ", end='')
+c = float(input())
+f = c * (float(9) / 5) + 32
+print(str(c) + " Celcius adalah : " + str(f) + " Farenheit")
